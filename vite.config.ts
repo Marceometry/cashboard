0d8179f79e79f6db4b31d4dc4b734dc53c5a350c
Marcelino Teixeira <marcelino.teixeira.dev@gmail.com>
2023-06-23 11:57:54 -0300

    feat: add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,62 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        name: 'Cashboard',
+        short_name: 'Cashboard',
+        description:
+          'Cashboard - registre e analise todos os seus ganhos e despesas da maneira que desejar.',
+        background_color: '#48bb78',
+        theme_color: '#48bb78',
+        icons: [
+          {
+            src: `pwa-64x64.png`,
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: `pwa-192x192.png`,
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: `pwa-512x512.png`,
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: `maskable-icon-512x512.png`,
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      includeAssets: [
+        `favicon.ico`,
+        `pwa-64x64.png`,
+        `pwa-192x192.png`,
+        `pwa-512x512.png`,
+        `maskable-icon-512x512.png`,
+        `apple-touch-icon-180x180.png`,
+      ],
+    }),
+  ],
 })
